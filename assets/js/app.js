ebd680e74cdadde25cbf30dab4443bf5cad9397e
manucarenjot <carenjotje@gmail.com>
2021-12-30 15:13:46 +0100

    Memory

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,35 @@
-let box =$('.box');
+let box1 =$('#box1');
+let box2 =$('#box2');
+let box3 =$('#box3');
+let box4 =$('#box4');
+
+let box = document.querySelectorAll('.box');
 let frontface =$('.frontFace');
 
 
 const icons = [
-    'fa-kiwi-bird',
-    'fa-chess',
-    'fa-frog',
-    'fa-camera-retro',
-
+    'https://res.cloudinary.com/beumsk/image/upload/v1547980025/memory/Pokemon/Bulbasaur.png',
+    'https://res.cloudinary.com/beumsk/image/upload/v1547980083/memory/Pokemon/Charmander.png',
+    'https://res.cloudinary.com/beumsk/image/upload/v1547980101/memory/Pokemon/Squirtle.png',
+    'https://res.cloudinary.com/beumsk/image/upload/v1547980116/memory/Pokemon/Pikachu.png',
+    'https://res.cloudinary.com/beumsk/image/upload/v1547980025/memory/Pokemon/Bulbasaur.png',
+    'https://res.cloudinary.com/beumsk/image/upload/v1547980083/memory/Pokemon/Charmander.png',
+    'https://res.cloudinary.com/beumsk/image/upload/v1547980101/memory/Pokemon/Squirtle.png',
+    'https://res.cloudinary.com/beumsk/image/upload/v1547980116/memory/Pokemon/Pikachu.png'
 ];
+
+
+
+function start() {
+    for (i=0; i<box.length; i++) {
+
+
+        let rand = Math.floor(Math.random() * (icons.length - 1));
+        box[i].innerHTML = "<img src='" + icons[rand] + "' alt='image'>";
+        icons.splice(rand, 1);
+    }
+}
+
+$('#start').click(function () {
+    start()
+})
