46ce7da0d18e40a7ae9ce3963e892b2bf5ed227b
manucarenjot <carenjotje@gmail.com>
2021-12-30 15:52:09 +0100

    Memory

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,21 +12,23 @@ let frontface =$('.frontFace');
 
 
 const icons = [
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980025/memory/Pokemon/Bulbasaur.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980083/memory/Pokemon/Charmander.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980101/memory/Pokemon/Squirtle.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980116/memory/Pokemon/Pikachu.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980025/memory/Pokemon/Bulbasaur.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980083/memory/Pokemon/Charmander.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980101/memory/Pokemon/Squirtle.png',
-    'https://res.cloudinary.com/beumsk/image/upload/v1547980116/memory/Pokemon/Pikachu.png'
+    'url(/assets/img/Bulbasaur.png)',
+    'url(/assets/img/Charmander.png)',
+    'url(/assets/img/pikachu.png)',
+    'url(/assets/img/Squirtle.png)',
+    'url(/assets/img/Bulbasaur.png)',
+    'url(/assets/img/Charmander.png)',
+    'url(/assets/img/pikachu.png)',
+    'url(/assets/img/Squirtle.png)'
+
+
 ];
 
 
 function start() {
     for (i=0; i<box.length; i++) {
         let rand = Math.floor(Math.random() * (icons.length - 1));
-        box[i].innerHTML = "<img src='" + icons[rand] + "' alt='image' class='hidden'>";
+        box[i].style.backgroundImage = icons[rand];
         icons.splice(rand, 1);
     }
 }
